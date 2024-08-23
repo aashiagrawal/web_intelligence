fe66aa65f80fdaff5772fa6228efcddededc1562
aashiagrawal <aashia2@illinois.edu>
2024-08-23 13:11:04 -0400

    updated prismaClient import

--- web_intelligence_app/app/api/webinfo/route.ts
+++ web_intelligence_app/app/api/webinfo/route.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 import { NextRequest, NextResponse } from "next/server";
 import { exec } from "child_process";
 import { promisify } from "util";
